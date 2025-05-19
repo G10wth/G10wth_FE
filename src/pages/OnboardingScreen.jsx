acf759e0353fdc415b61facd0d3bdba117ff10c9
Dubabbi <thdms3160@naver.com>
2025-05-19 09:41:50 +0900

    feat: 온보딩 페이지 구현

---
+++ src/pages/OnboardingScreen.jsx
@@ -0,0 +1,28 @@
+import Logo from '/public/logo.svg';
+import LogoTitle from '@/assets/images/logo-title-img.png';
+
+export default function WelcomeScreen() {
+  return (
+    <div className="min-h-screen h-full overflow-hidden w-full bg-gradient-to-b from-[#FAC61A] via-[#F6931E] to-[#FE7615] flex flex-col justify-between items-center pt-24 pb-10">
+      <div className="flex flex-col items-center gap-4">
+        <img src={Logo} alt="로고" />
+
+        <div className="w-52 flex flex-col items-center gap-4">
+          <p className="text-center text-black text-base font-semibold leading-snug">
+            쉽게 갈 수 있는 장소
+          </p>
+          <img className="h-8 w-full object-contain" src={LogoTitle} alt="EasyPlace 로고" />
+        </div>
+      </div>
+
+      <div className="w-full px-6 flex flex-col gap-3 items-center">
+        <button className="bg-[#FEE500] text-black text-base font-semibold w-full max-w-xs py-3 rounded-full shadow-md">
+          카카오로 계속하기
+        </button>
+        <button className="text-black text-sm font-semibold underline underline-offset-2">
+          이메일로 로그인 / 회원가입
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/routes/mainRoutes.jsx
+++ src/routes/mainRoutes.jsx
@@ -6,6 +6,7 @@ const MyPage = lazy(() => import('@/pages/MyPage'));
 const Layout = lazy(() => import('@/layouts/Layout'));
 const ErrorPage = lazy(() => import('@/pages/ErrorPage'));
 const NotFound = lazy(() => import('@/pages/NotFound'));
+const OnboardingScreen = lazy(() => import('@/pages/OnboardingScreen'));
 const LoopLoading = lazy(() => import('@/components/common/LoopLoading'));
 
 export default function MainRoutes() {
@@ -15,6 +16,7 @@ export default function MainRoutes() {
         <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
           <Route index element={<Main />} />
           <Route path="mypage" element={<MyPage />} />
+          <Route path="/onboarding" element={<OnboardingScreen />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
