217d6a6d57237c0dcca59fe972f2eb17aaf83bf1
Dubabbi <thdms3160@naver.com>
2025-05-19 21:00:33 +0900

    feat: 지도 토글 바텀 시트 구현

--- src/components/main/BottomSheetFilter.jsx
+++ src/components/main/BottomSheetFilter.jsx
@@ -1,36 +1,39 @@
+import { useState } from 'react';
+
 import ViewToggle from './ViewToggle';
-import {
-  PiWheelchair,
-  PiToilet,
-  PiDotsThreeCircle,
-  PiSpeakerHigh,
-  PiMountains,
-} from 'react-icons/pi';
+import AccessibilityFilterButton from './AccessibilityFilterButton';
+
+import { PiWheelchair, PiToilet, PiSpeakerHigh, PiMountains } from 'react-icons/pi';
+
+import { FiEye } from 'react-icons/fi';
 
 export default function BottomSheetFilter({ selected, onChange }) {
+  const [activeFilter, setActiveFilter] = useState(null);
   const filters = [
     { icon: <PiWheelchair size={18} />, label: '휠체어 출입 가능' },
     { icon: <PiToilet size={18} />, label: '장애인 화장실' },
-    { icon: <PiDotsThreeCircle size={18} />, label: '점자 메뉴' },
+    { icon: <FiEye size={18} />, label: '점자 메뉴' },
     { icon: <PiSpeakerHigh size={18} />, label: '오디오 가이드' },
     { icon: <PiMountains size={18} />, label: '경사로' },
   ];
 
   return (
-    <div className="absolute bottom-0 w-full px-4 pt-6 pb-8 bg-white rounded-t-3xl shadow-[0px_-2px_4px_rgba(0,0,0,0.1)]">
-      <h2 className="text-base font-bold mb-4">접근성 필터</h2>
-      <div className="flex flex-wrap gap-2 mb-6">
+    <div className="fixed max-w-[425px] mx-auto z-20 bottom-0 px-6 pt-6 pb-8 bg-white rounded-t-3xl shadow-[0px_-2px_4px_rgba(0,0,0,0.1)]">
+      <h2 className="text-base text-black font-bold mb-4">접근성 필터</h2>
+      <div className="flex text-left flex-wrap gap-2 mb-6">
         {filters.map((item, i) => (
-          <button
+          <AccessibilityFilterButton
             key={i}
-            className="flex items-center gap-1 px-3 py-2 border border-gray-300 rounded-full text-sm text-black"
-          >
-            {item.icon}
-            {item.label}
-          </button>
+            icon={item.icon}
+            label={item.label}
+            isActive={activeFilter === item.label}
+            onClick={() => setActiveFilter(item.label)}
+          />
         ))}
       </div>
-      <ViewToggle selected={selected} onChange={onChange} />
+      <div className="flex justify-center">
+        <ViewToggle selected={selected} onChange={onChange} />
+      </div>
     </div>
   );
 }
