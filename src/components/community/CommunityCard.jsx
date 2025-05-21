4b11eca17289d72c6e590b22fd7da58adb91aa9e
Dubabbi <thdms3160@naver.com>
2025-05-21 15:47:26 +0900

    feat: 수정/삭제 모달 기본 구성

--- src/components/community/CommunityCard.jsx
+++ src/components/community/CommunityCard.jsx
@@ -1,29 +1,44 @@
 import { useState } from 'react';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
 import { FaRegCommentDots } from 'react-icons/fa';
+import CommentModal from './CommentModal';
 
 const CommunityCard = ({ username, userHandle, profileImage, content, image }) => {
   const [liked, setLiked] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className="w-full h-auto relative p-3">
-      <div className="flex items-center space-x-2">
-        <img src={profileImage} alt="profile" className="w-7 h-7 rounded-full" />
-        <div>
-          <div className="text-xs font-medium text-black">{username}</div>
-          <div className="text-xs text-zinc-400">{userHandle}</div>
+    <div className="w-full h-auto relative p-3 border-b border-gray-100">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-2">
+          <img src={profileImage} alt="profile" className="w-7 h-7 rounded-full" />
+          <div>
+            <div className="text-xs font-medium text-black">{username}</div>
+            <div className="text-xs text-zinc-400">{userHandle}</div>
+          </div>
         </div>
-        <div className="absolute top-2 right-2 flex space-x-1">
-          <div className="w-[2.36px] h-[2.36px] bg-black rounded-full" />
-          <div className="w-[2.36px] h-[2.36px] bg-black rounded-full" />
-          <div className="w-[2.36px] h-[2.36px] bg-black rounded-full" />
+
+        <div className="relative">
+          <button
+            onClick={() => setShowModal(prev => !prev)}
+            className="flex flex-col justify-center items-center w-6 h-6"
+          >
+            <div className="w-[2.5px] h-[2.5px] bg-black rounded-full mb-[2px]" />
+            <div className="w-[2.5px] h-[2.5px] bg-black rounded-full mb-[2px]" />
+            <div className="w-[2.5px] h-[2.5px] bg-black rounded-full" />
+          </button>
+
+          {showModal && (
+            <div className="absolute top-6 right-full mr-2">
+              <CommentModal onClose={() => setShowModal(false)} />
+            </div>
+          )}
         </div>
       </div>
 
       <p className="mt-3 text-xs text-neutral-500 font-medium leading-snug">{content}</p>
 
       <img src={image} alt="content" className="mt-4 w-full h-40 object-cover rounded-2xl" />
-
       <div className="mt-2 flex space-x-4">
         <button onClick={() => setLiked(prev => !prev)}>
           {liked ? (
