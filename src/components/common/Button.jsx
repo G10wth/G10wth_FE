a5aa87bd91925296caebfbb2c579086648135a93
cjiyun <chlwldbs78@gmail.com>
2025-05-21 14:29:35 +0900

    style: 버튼 색 수정

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,8 +2,8 @@ const Button = ({ text, onClick, disabled = false }) => {
   return (
     <button
       onClick={onClick}
-      className={`w-full py-3.5 rounded-[8px]
-        ${disabled ? 'bg-gray-300 text-gray-500' : 'bg-orangeStrong text-white'}`}
+      className={`w-full py-4 rounded-[8px]
+        ${disabled ? 'bg-gray-200 text-gray200 font-medium' : 'bg-orangeStrong text-white'}`}
     >
       {text}
     </button>
