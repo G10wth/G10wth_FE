366812099287efb1af4a8668f4d6ebea435c221b
cjiyun <chlwldbs78@gmail.com>
2025-05-21 11:58:20 +0900

    style: 유효성 검사에 따른 버튼 스타일 변경

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,6 +1,10 @@
-const Button = ({ text, onClick }) => {
+const Button = ({ text, onClick, disabled = false }) => {
   return (
-    <button onClick={onClick} className="w-full py-3.5 rounded-[8px] bg-orangeStrong text-white">
+    <button
+      onClick={onClick}
+      className={`w-full py-3.5 rounded-[8px]
+        ${disabled ? 'bg-gray-300 text-gray-500' : 'bg-orangeStrong text-white'}`}
+    >
       {text}
     </button>
   );
