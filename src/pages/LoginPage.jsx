42fbe017b8a80edeb211ee8b97a22a012a367576
cjiyun <chlwldbs78@gmail.com>
2025-05-21 15:28:35 +0900

    feat: 안내문구 p태그 AuthHeader로 컴포넌트 분리 및 로그인/회원가입 페이지 조건부 렌더링 추가

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import SocialKakaoLogin from '@/components/login/SocialKakao';
 import DirectLoginForm from '@/components/login/DirectLoginForm';
 import useAutoRedirect from '@/hooks/useAutoRedirect';
+import AuthHeader from '@/components/common/AuthHeader';
 import TopBar from '@/components/common/TopBar';
 
 const LoginPage = () => {
@@ -12,10 +13,7 @@ const LoginPage = () => {
     <div>
       <TopBar title="로그인" />
       <div className="fixed max-w-[425px] w-full top-[60px] flex flex-col items-center justify-center gap-8 p-4">
-        <div className="w-full space-y-1">
-          <p className="text-black text-2xl font-bold">EasyPlace와 함께</p>
-          <p className="text-black text-2xl font-bold">쉽게 갈 수 있는 장소로 바로!</p>
-        </div>
+        <AuthHeader step={0} />
         <DirectLoginForm autoLogin={autoLogin} setAutoLogin={setAutoLogin} />
         <div className="w-full flex items-center gap-4">
           <hr className="flex-grow border-t border-gray100" />
